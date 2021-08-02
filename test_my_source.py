f9b92149c0560ddbef9c6f5fd3d67a9deff902c5
GaryeongKim <83697282+GaryeongKim@users.noreply.github.com>
2021-08-02 12:23:20 +0200

    Update test_my_source.py

--- test_my_source.py
+++ test_my_source.py
@@ -5,7 +5,8 @@ from my_source import add_func
 def test_add_one():
     assert add_func(2, 5) == 7
 
-#nicht funktionierende Test
+#nicht funktionierende Test (falsches Ergebnis werfen)
+#xfail = expected fail
 @pytest.mark.xfail
 def test_add_fail():
     assert add_func(5, 5) == 9
